ecc6d79ee7586601adf6ca81368f14d07d630b1b
Vasili <anoshin45@gmail.com>
2019-01-12 21:16:11 -0800

    add componentDidMount() that fetch all the Data from server

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchPage from './SearchPage.js'
 import {Link} from 'react-router-dom'
 import {Route} from 'react-router-dom'
 import BookLibrary from './BookLibrary.js'
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books : []
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll()
+    .then((books) =>{
+      this.setState(() => ({
+        books
+      }))
+    })
   }
 
   render() {
